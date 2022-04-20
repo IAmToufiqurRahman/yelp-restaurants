9ab4d869af65ce2d86f50c788593d59890f4e572
IAmToufiqurRahman <toufiqurrahmanaust@gmail.com>
2022-04-20 13:18:42 +0600

    included catch all in server, now ready to deploy

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')))
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'))
+})
+
 app.listen(port, () => {
   console.log(`App listening on ${port}`)
 })
